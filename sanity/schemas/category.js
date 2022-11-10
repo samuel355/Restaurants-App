2ae369a8b708920e5d18d6b5341a13b0de616016
Samuel Osei Adu <asamuel355@yahoo.com>
2022-11-10 18:27:15 +0000

    sanity studio and data

---
+++ sanity/schemas/category.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'category',
+  title: 'Menu Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Category Name',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image of Category'
+    }
+  
+  ],
+}
---
+++ sanity/schemas/dish.js
@@ -0,0 +1,30 @@
+export default {
+    name: 'dish',
+    title: 'Dish',
+    type: 'document',
+    fields : [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name of Dish',
+            validation: (Rule) => Rule.required()
+        },
+        {
+            name: 'short_description',
+            type: 'string',
+            title: 'Short Description',
+            validation: (Rule) => Rule.max(200)
+        },
+        {
+            name: 'price',
+            type: 'number',
+            title: 'Price of dish GHS',
+            validation: (Rule) => Rule.required()
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image of dish'
+        }
+    ]
+}
